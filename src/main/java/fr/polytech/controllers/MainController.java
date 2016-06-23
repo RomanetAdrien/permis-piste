f2f7489481e3929968f5ff9aada16019b881e5ed
J0hn <jonathan.platteau@yahoo.fr>
2016-06-23 16:01:26 +0200

    Enfin !
    Page d'acceuil + exemples de pages dynamiques

--- src/main/java/fr/polytech/controllers/MainController.java
+++ src/main/java/fr/polytech/controllers/MainController.java
@@ -1,7 +1,9 @@
 package fr.polytech.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -11,9 +13,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     @RequestMapping("/")
+    public String home()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
     @ResponseBody
-    String home() {
+    public String hello()
+    {
         return "Hello World !";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
 
