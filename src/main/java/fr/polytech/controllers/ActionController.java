f5583a98e72f5fea135379590bc7b5400604c60b
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-06-26 16:23:56 +0200

    Controller base Action

--- src/main/java/fr/polytech/controllers/ActionController.java
+++ src/main/java/fr/polytech/controllers/ActionController.java
@@ -6,7 +6,10 @@ import fr.polytech.models.ActionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by FloAdmin on 26/06/2016.
@@ -18,6 +21,36 @@ public class ActionController {
     public String index(Model model) {
         model.addAttribute("action", actionDao.findAll());
         return "action";
+    }
+
+    @RequestMapping("/action-new")
+    public String newAction(Model model){
+        model.addAttribute("Action", new Action());
+        return "nouvelleAction";
+    }
+
+    @RequestMapping(value="/action-create", method = RequestMethod.POST)
+    public String create(@ModelAttribute Action action, Model model){
+        try {
+            actionDao.save(action);
+
+        }
+        catch (Exception ex ) {
+            return "Error creating action :" + ex.toString();
+        }
+        return"redirect:/action";
+    }
+
+    @RequestMapping("/action-supprimer")
+    public String delete(@RequestParam(value ="id")int id){
+        try {
+            actionDao.delete(actionDao.findBynumact(id));
+        }
+        catch (Exception ex) {
+            return "Error deleting Action :" + ex.toString();
+
+        }
+        return "redirect:/action";
     }
 
     @Autowired
--- src/main/java/fr/polytech/models/ActionDao.java
+++ src/main/java/fr/polytech/models/ActionDao.java
@@ -13,4 +13,5 @@ public interface ActionDao extends CrudRepository<Action,Long> {
 
 
     Iterable<Action> findAll();
+    Action findBynumact(int numaction);
 }
